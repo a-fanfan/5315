735ee0c5a519fd33676cecb4538c986055b2d171
项一凡 <xiangyifan@xiangyifandeMacBook-Pro.local>
2017-10-04 17:00:05 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -1,124 +1,64 @@
 {
   "data": [
     {
-      "name": "Garrett Winters",
-      "designation": "Accountant",
-      "salary": "$170,750",
-      "joining_date": "2011/07/25",
+      "name": "Bowen Chen",
+      "phone": "111111111",
+      "email": "chenb2@seattleu.edu",
+      "office": "CHINA",
+    },
+   {
+      "name": "Kiki Li",
+      "phone": "222222222",
+      "email": "lix1@seattleu.edu",
       "office": "Tokyo",
-      "extension": "8422"
-    },
+    },   
+     {
+      "name": "Qingyue Li",
+      "phone": "333333333",
+      "email": "liq4@seattleu.edu",
+      "office": "USA",
+    },   
     {
-      "name": "Brielle Williamson",
-      "designation": "Integration Specialist",
-      "salary": "$372,000",
-      "joining_date": "2012/12/02",
-      "office": "New York",
-      "extension": "4804"
+      "name": "Nancy Xu",
+      "phone": "444444444",
+      "email": "Xuy13@seattleu.edu",
+      "office": "UK",
     },
-    {
-      "name": "Ashton Cox",
-      "designation": "Junior Technical Author",
-      "salary": "$86,000",
-      "joining_date": "2009/01/12",
-      "office": "San Francisco",
-      "extension": "1562"
+     {
+      "name": "Baojing Shi",
+      "phone": "555555555",
+      "email": "shib1@seattleu.edu",
+      "office": "CANADA",
     },
-    {
-      "name": "Airi Satou",
-      "designation": "Accountant",
-      "salary": "$162,700",
-      "joining_date": "2008/11/28",
+     {
+      "name": "Yifan Xiang",
+      "phone": "666666666",
+      "email": "xiangy@seattleu.edu",
+      "office": "UK",
+    },
+     {
+      "name": "David Kuang",
+      "phone": "777777777",
+      "email": "kuangc@seattle.com",
+      "office": "USA",
+    },
+     {
+      "name": "Katie",
+      "phone": "888888888",
+      "email": "jiy2@seattleu.edu",
+      "office": "CHINA",
+    },
+     {
+      "name": "Zeyang Wu",
+      "phone": "999999999",
+      "email": "wuz7@seattleu.edu",
       "office": "Tokyo",
-      "extension": "5407"
     },
-    {
-      "name": "Caesar Vance",
-      "designation": "Pre-Sales Support",
-      "salary": "$106,450",
-      "joining_date": "2011/12/12",
-      "office": "New York",
-      "extension": "8330"
-    },
-    {
-      "name": "Shad Decker",
-      "designation": "Regional Director",
-      "salary": "$183,000",
-      "joining_date": "2008/11/13",
-      "office": "Edinburgh",
-      "extension": "6373"
-    },
-    {
-      "name": "Cedric Kelly",
-      "designation": "Senior Javascript Developer",
-      "salary": "$433,060",
-      "joining_date": "2012/03/29",
-      "office": "Edinburgh",
-      "extension": "6224"
-    },
-    {
-      "name": "Haley Kennedy",
-      "designation": "Senior Marketing Designer",
-      "salary": "$313,500",
-      "joining_date": "2012/12/18",
-      "office": "London",
-      "extension": "3597"
-    },
-    {
-      "name": "Colleen Hurst",
-      "designation": "Javascript Developer",
-      "salary": "$205,500",
-      "joining_date": "2009/09/15",
-      "office": "San Francisco",
-      "extension": "2360"
-    },
-    {
-      "name": "Dai Rios",
-      "designation": "Personnel Lead",
-      "salary": "$217,500",
-      "joining_date": "2012/09/26",
-      "office": "Edinburgh",
-      "extension": "2290"
-    },
-    {
-      "name": "Herrod Chandler",
-      "designation": "Sales Assistant",
-      "salary": "$137,500",
-      "joining_date": "2012/08/06",
-      "office": "San Francisco",
-      "extension": "9608"
-    },
-    {
-      "name": "Rhona Davidson",
-      "designation": "Integration Specialist",
-      "salary": "$327,900",
-      "joining_date": "2010/10/14",
+     {
+      "name": "Yun",
+      "phone": "000000000",
+      "email": "liuy33@seattleu.edu",
       "office": "Tokyo",
-      "extension": "6200"
     },
-    {
-      "name": "Sonya Frost",
-      "designation": "Software Engineer",
-      "salary": "$103,600",
-      "joining_date": "2008/12/13",
-      "office": "Edinburgh",
-      "extension": "1667"
-    },
-    {
-      "name": "Jena Gaines",
-      "designation": "Office Manager",
-      "salary": "$90,560",
-      "joining_date": "2008/12/19",
-      "office": "London",
-      "extension": "3814"
-    },
-    {
-      "name": "Quinn Flynn",
-      "designation": "Support Lead",
-      "salary": "$342,000",
-      "joining_date": "2013/03/03",
-      "office": "Edinburgh",
-      "extension": "9497"
-    }
   ]
 }

f7f57d6345b49bb1bfa237c0babc672b1122346d
项一凡 <xiangyifan@xiangyifandeMacBook-Pro.local>
2017-10-04 19:56:41 -0700

    hw1 commit

--- empdata.js
+++ empdata.js
@@ -4,61 +4,61 @@
       "name": "Bowen Chen",
       "phone": "111111111",
       "email": "chenb2@seattleu.edu",
-      "office": "CHINA",
+      "office": "CHINA"
     },
    {
       "name": "Kiki Li",
       "phone": "222222222",
       "email": "lix1@seattleu.edu",
-      "office": "Tokyo",
+      "office": "Tokyo"
     },   
      {
       "name": "Qingyue Li",
       "phone": "333333333",
       "email": "liq4@seattleu.edu",
-      "office": "USA",
+      "office": "USA"
     },   
     {
       "name": "Nancy Xu",
       "phone": "444444444",
       "email": "Xuy13@seattleu.edu",
-      "office": "UK",
+      "office": "UK"
     },
      {
       "name": "Baojing Shi",
       "phone": "555555555",
       "email": "shib1@seattleu.edu",
-      "office": "CANADA",
+      "office": "CANADA"
     },
      {
       "name": "Yifan Xiang",
       "phone": "666666666",
       "email": "xiangy@seattleu.edu",
-      "office": "UK",
+      "office": "UK"
     },
      {
       "name": "David Kuang",
       "phone": "777777777",
       "email": "kuangc@seattle.com",
-      "office": "USA",
+      "office": "USA"
     },
      {
       "name": "Katie",
       "phone": "888888888",
       "email": "jiy2@seattleu.edu",
-      "office": "CHINA",
+      "office": "CHINA"
     },
      {
       "name": "Zeyang Wu",
       "phone": "999999999",
       "email": "wuz7@seattleu.edu",
-      "office": "Tokyo",
+      "office": "Tokyo"
     },
      {
       "name": "Yun",
       "phone": "000000000",
       "email": "liuy33@seattleu.edu",
-      "office": "Tokyo",
-    },
+      "office": "Tokyo"
+    }
   ]
 }
